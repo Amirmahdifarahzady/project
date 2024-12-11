e70c4e358db0fb9f4611579f321c14fa20dca9a0
Amirmahdifarahzady <amyrmhdyfrhzady@gmail.com>
2024-12-11 08:57:41 +0330

    project-teacher-sohrabi-1.0.0

---
+++ main.js
@@ -0,0 +1,7 @@
+let a = +prompt("write a number1");
+let b = +prompt("write a number2");
+let c = +prompt("write a number3");
+let d = +prompt("write a number4");
+document.write(a + b + c + d);
+console.log(a + b + c + d);
+alert(a + b + c + d);
